0aa7d6e10221cfbcbeb7b028769cfb62d47597e7
jmillana <jordi@marimon.tech>
2022-10-02 23:24:35 +0200

    Create __init__.py

---
+++ htbengine/filters/__init__.py
@@ -0,0 +1,8 @@
+"""Filter subpackage.
+
+This subpackage contains all the filters for the HTBEngine.
+    * machines: The filters for the machines
+    * users: The filters for the users
+"""
+from .machines import *  # noqa: F401, F403
+from .users import *  # noqa: F401, F403
