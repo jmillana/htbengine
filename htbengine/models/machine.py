a8303ee9779d075777794a509b091888eb6c91a5
jmillana <jordi@marimon.tech>
2022-10-02 23:27:38 +0200

    Add HTB machine datamodel

---
+++ htbengine/models/machine.py
@@ -0,0 +1,86 @@
+"""HTB machine model."""
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass, field
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class FeedbackChart:
+    """Represents the feedback charts on HTB machines."""
+
+    counterCake: int
+    counterVeryEasy: int
+    counterEasy: int
+    counterTooEasy: int
+    counterMedium: int
+    counterBitHard: int
+    counterHard: int
+    counterTooHard: int
+    counterExHard: int
+    counterBrainFuck: int
+
+
+@dataclass
+class PlayInfo:
+    """Representa the player information on the HTB machines."""
+
+    isSpawned: bool | None = None
+    isSpawning: bool | None = None
+    isActive: bool | None = None
+    active_player_count: int | None = None
+    expires_at: datetime | None = None
+
+
+@dataclass
+class Maker:
+    """Represents the authors of the HTB machines."""
+
+    id: int
+    name: str
+    avatar: str
+    isRespected: bool
+
+
+@dataclass
+class Tag:
+    """Represents the tags on the HTB machines."""
+
+    id: int
+    tag_category_id: int
+
+
+@dataclass
+class HTBMachine:
+    """Represents the information that any HTB machine have."""
+
+    id: int | str
+    name: str
+    avatar: str
+    ip: str
+    os: str
+    points: int | str
+    static_points: int | str
+    release: datetime | str
+    difficulty: int | str
+    difficultyText: str
+    feedbackForChart: FeedbackChart
+    playInfo: PlayInfo
+    stars: float | str
+    user_owns_count: int | str
+    root_owns_count: int | str
+    authUserHasReviewed: bool | str
+    isTodo: bool | str
+    easy_month: int | str
+    sp_flag: int | str
+    recommended: int | str
+    authUserInUserOwns: bool | None | str
+    authUserInRootOwns: bool | None | str
+    free: bool | str
+    maker: Maker
+    maker2: Maker | None = None
+    tags: list[Tag] = field(default_factory=list)
+    active: bool | str = False
