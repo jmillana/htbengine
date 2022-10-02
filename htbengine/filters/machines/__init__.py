9be5eeebe2eb7dad2c1586ed8dcf42071727deb9
jmillana <jordi@marimon.tech>
2022-10-02 23:24:44 +0200

    Create __init__.py

---
+++ htbengine/filters/machines/__init__.py
@@ -0,0 +1,10 @@
+"""Machine filter module.
+
+This module contains the machine filter functions.
+"""
+
+from .free_filter import filter_by_free  # noqa: F401
+from .os_filter import filter_by_os  # noqa: F401
+from .owned_filter import filter_by_owned  # noqa: F401
+from .spawned_filter import filter_by_spawned  # noqa: F401
+from .status_filter import filter_by_status  # noqa: F401
