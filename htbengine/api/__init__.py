0372ee914770625d28443b962b330db2309144e2
jmillana <jordi@marimon.tech>
2022-10-02 23:24:12 +0200

    Create __init__.py

---
+++ htbengine/api/__init__.py
@@ -0,0 +1,7 @@
+"""HTB API submodules.
+
+This module contains the submodules of the HTB API.
+    * endpoints: The HTB API endpoints
+    * machines: The HTB API machines
+    * users: The HTB API users
+"""
