6e51331e92a0bc8cf56c0fcbaffb6c943b234f2b
jmillana <jordi@marimon.tech>
2022-10-03 22:48:46 +0200

    Update __init__.py

--- htbengine/filters/machines/__init__.py
+++ htbengine/filters/machines/__init__.py
@@ -6,6 +6,8 @@ This module contains the machine filter functions.
 from .difficulty_filter import filter_by_difficulty  # noqa: F401
 from .filter_machines import filter_machines  # noqa: F401
 from .free_filter import filter_by_free  # noqa: F401
+from .ip_filter import ip_filter  # noqa: F401
+from .name_filter import name_filter  # noqa: F401
 from .os_filter import filter_by_os  # noqa: F401
 from .owned_filter import filter_by_owned  # noqa: F401
 from .spawned_filter import filter_by_spawned  # noqa: F401
