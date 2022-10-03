2a1b113aeaa6ba33dce6680b1f0008f3aef95e78
jmillana <jordi@marimon.tech>
2022-10-03 21:04:38 +0200

    Update __init__.py

--- htbengine/models/__init__.py
+++ htbengine/models/__init__.py
@@ -3,4 +3,6 @@
 This package contains all the models used by the application.
     * HTBMachine: The model for the machine
 """
+from . import difficulty  # noqa: F401
 from . import machine  # noqa: F401
+from . import os  # noqa: F401
